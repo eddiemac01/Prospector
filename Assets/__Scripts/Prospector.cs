5f7d8f8b4167de6775ddda35735f74f1cb88e41b
eddiemac01 <eddiemac01@gmail.com>
2016-03-29 01:10:22 -0400

    Prospector first commit

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -4,19 +4,38 @@ using System.Collections.Generic;
 
 
 public class Prospector : MonoBehaviour {
-
 	static public Prospector 	S;
+
 	public Deck					deck;
 	public TextAsset			deckXML;
+
+	public Layout				layout;
+	public TextAsset			layoutXML;
 
 	void Awake(){
 		S = this;
 	}
 
+	public List<CardProspector>		drawPile;
+
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
 		Deck.Shuffle (ref deck.cards);
+
+		layout = GetComponent<Layout> ();	//get the layout
+		layout.ReadLayout (layoutXML.text);	//pass LayoutXML to it
+
+		drawPile = ConvertListCardsToListCardProspectors (deck.cards);
 	}
 
+	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD) {
+		List<CardProspector> lCP = new List<CardProspector> ();
+		CardProspector tCP;
+		foreach (Card tCD in lCD) {
+			tCP = tCD as CardProspector;
+			lCP.Add (tCP);
+		}
+		return(lCP);
+	}
 }
